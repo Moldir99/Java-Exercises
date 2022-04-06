6ed9553bc56b7de1ef7ad53a09de1cc3557358fa
Moldir <51477205+Moldir99@users.noreply.github.com>
2022-04-06 18:29:05 +0300

    Add files via upload

---
+++ Ch_01_1-7.java
@@ -0,0 +1,88 @@
+/*
+// Ex #1: (Print statments below);
+class Main{
+  public static void main(String[] args){
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Computer Science");
+    System.out.println("Programming is fun");
+  }
+}
+*/
+
+/*
+// Ex #2: (Print string 5 times);
+class Main{
+  public static void main(String[] args){
+    String str = "Welcome to Java";
+    int n = 5;
+    for(int i = 1; i <= n; i++){
+      System.out.println(str);
+    }
+  }
+}
+*/
+
+/*
+// Ex #3: (Display JAVA);
+class Main{
+  public static void main(String[] args){
+    System.out.println("    J" + "    A" + "    V     V" + "    A");
+    System.out.println("    J" + "   A A" + "    V   V" + "    A A");
+    System.out.println("J   J" + "  AAAAA" + "    V V" + "    AAAAA");
+    System.out.println(" J J" + "  A     A" + "    V" + "    A     A");
+  }
+}
+*/
+
+/*
+// Ex #4: (Display table);
+
+class Main{
+  public static void main(String[] args){
+    System.out.println("a" + "    a^2" + "    a^3");
+    System.out.println("1" + "    1" + "      1");
+    System.out.println("2" + "    4" + "      8");
+    System.out.println("3" + "    9" + "      27"); 
+    System.out.println("4" + "    16" + "     64");
+  }
+}
+*/
+
+/*
+// Ex #5: (Display solution of eq.);
+
+class Main{
+  public static void main(String[] args){
+    System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+  }
+}
+*/
+
+/*
+// Ex #6: (Display the sum of numbers from 1 to 9);
+
+class Main{
+  public static void main(String[] args){
+    int sum = 0;
+    for (int i = 1; i <= 9; i++){
+      sum += i;
+    }
+    System.out.println("The sum of numbers from 1 to 9 is " + sum);
+  }
+}
+*/
+
+/*
+// Ex #7: (Compute pi number):
+
+class Main{
+  public static void main(String[] args){
+    double pi = 4 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+    double pi2 = 4 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11) + (1.0/13));
+    System.out.println(pi);
+    System.out.println(pi2);
+    System.out.println(Math.PI);
+  }
+}
+*/
+
